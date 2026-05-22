fc4901f1e121b8ddd4b05c14647670b3dd8100c8
gschhh <alexshashin.2023@gmail.com>
2026-05-22 14:50:14 +0500

    Прослушиватель местоположения пользователя. Шашин

--- app/src/main/java/com/example/maps/MainActivity.java
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.maps;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.runtime.image.ImageProvider;
+
 public class MainActivity extends AppCompatActivity {
+    LocationManager _LocationManager;
+    TextView textAddress;
+    MapView mapView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +35,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    LocationListener _locationListner = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if(location != null) {
+                mapView.getMap().move(
+                        new CameraPosition(
+                                new Point(location.getLatitude(), location.getLongitude()), 15, 0,0));
+                mapView.getMap().getMapObjects().clear();
+                mapView.getMap().getMapObjects().addPlacemark(
+                        new Point(location.getLatitude(), location.getLongitude()),
+                        ImageProvider.fromResource(location.getLongitude()) + "," + String.valueOf(location.getLatitude()),
+                        textAddress
+                );
+                getAddressByGPS.execute();
+            }
+        }
+    };
 }
